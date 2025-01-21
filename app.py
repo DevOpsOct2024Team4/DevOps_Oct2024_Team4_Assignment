811e9a72dab9c5141413f3be796219ba569586bf
Shawn <115801000+Sty19@users.noreply.github.com>
2025-01-21 11:27:27 +0800

    Added initial details to app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 
+# Initialize the Flask app
 app = Flask(__name__)
+app.secret_key = "DevOps_key_Team4"  # For session and flash messages
+
+# Sample Matriculation List (Case Study)
+students = [
+    {"id": "A1234567X", "name": "John Tan", "points": 50},
+    {"id": "A1234568Y", "name": "Sarah Lim", "points": 80}
+]
+redeemable_items = [
+    {"name": "Item1", "quantity": 10, "value": 100},
+    {"name": "Item2", "quantity": 5, "value": 200}
+]
+
+# Page Routes
 
 @app.route("/")
 def home():
-    return "Flask 3.1.0 is running!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    """Landing page."""
+    return render_template("index.html")
